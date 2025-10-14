cca255a814724d2e0b6217a61049636362a0930d
Dima Karpenko dimak <dimka2010@gmail.com>
2025-10-14 13:50:16 +0300

    CustomerController

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using Dispancer.Configuration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace Dispancer.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize] // <-- єто важно - только авторизированные пользователи могут сюда попасть
+public class CustomerController : ControllerBase
+{
+     private readonly ConnectionStrings _connectionStrings;
+
+    public CustomerController(ConnectionStrings connectionStrings)
+    {
+        _connectionStrings = connectionStrings;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCustomers()
+    {
+        try
+        {
+            // используем строку подключении из appsettings.json
+            using (var connection = new SqlConnection(_connectionStrings.DefaultConnection))
+            {
+                // просто получаем всех пациентов из представления vGetCustomers
+                var customers = await connection.QueryAsync("SELECT * FROM dbo.vGetCustomers");
+                return Ok(customers);
+            }
+        }
+        catch (SqlException ex)
+        {
+            // В продакшен приложении здусь будет логирование ошибок
+            return StatusCode(500, new { message = "An internal server error occurred." });
+        }        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -30,6 +30,7 @@ builder.Services.AddAuthentication(options =>
         ValidateAudience = true,
         ValidateLifetime = true,
         ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings.Issuer, // <-- ÄÎÁÀÂÜÒÅ ÝÒÓ ÑÒÐÎÊÓ
         ValidAudience = jwtSettings.Audience,
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Key))
     };
