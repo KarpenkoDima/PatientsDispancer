2cde4a7f7950eb9719bc3983136b374a5a5c2be1
Dima Karpenko dimak <dimka2010@gmail.com>
2025-10-29 14:03:19 +0200

    Rewrite Record Med card of patirent (customer) - redesign med card - added Api Controller for dictionary Lands

---
+++ Controllers/DictionaryController.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using Dispancer.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace Dispancer.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize] // <-- это важно - только авторизированные пользователи могут сюда попасть
+public class DictionaryController : Controller
+{
+    private readonly UserConnectionService _connectionService;
+
+    public DictionaryController(UserConnectionService connectionService)
+    {
+        _connectionService = connectionService;// ТУТ ДАННЫЕ ДЛЯ ПОДКЛЮЧЕНИЯ
+    }
+
+    [HttpGet]
+    [Route("lands")]
+    [Authorize(Roles ="Sensitive_low,Sensitive_high,Sensitive_medium")]
+    public async Task<IActionResult> GetLangs()
+    {
+        try
+        {
+            // используем строку подключении из appsettings.json
+            using (var connection = _connectionService.CreateConnection())
+            {
+                // просто получаем всех пациентов из представления vGetCustomers
+                var lands = await connection.QueryAsync("SELECT * FROM dbo.vGetLand");
+                return Ok(lands);
+            }
+        }
+        catch (SqlException ex)
+        {
+            // В продакшен приложении здусь будет логирование ошибок
+            return StatusCode(500, new { message = "An internal server error occurred." });
+        }
+        catch (InvalidOperationException ex) // для ошибки если допустим отсутствует пароль или логин
+        {
+            return Unauthorized(new { message = ex.Message });
+        }
+    }
+}
--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -84,28 +84,37 @@ public class RegisterController : Controller
 
     // PUT api/customer/5/register/10 (Обновить запись с RegisterID=10
     [HttpPut("{entryId}")]
-    [Authorize(Roles ="Sensitive_medium,Sensitive_high")]
+    [Authorize(Roles = "Sensitive_medium,Sensitive_high")]
     public async Task<IActionResult> UpdateRegisterEntry(int customerId, int entryId, [FromBody] RegisterEntry registerEntry)
     {
-        using (var connection = _connectionService.CreateConnection())
+        try
         {
-            var parameters = new DynamicParameters();
-            parameters.Add("@RegisterID", entryId);
-            parameters.Add("@CustomerID", customerId);
-            parameters.Add("@FirstRegister", registerEntry.FirstRegister);
-            parameters.Add("@FirstDeregister", registerEntry.FirstDeregister);
-            parameters.Add("@SecondRegister", registerEntry.SecondRegister);
-            parameters.Add("@SecondDeRegister", registerEntry.SecondDeRegister);
-            parameters.Add("@Diagnosis", registerEntry.Diagnosis);
-            parameters.Add("@RegisterTypeID", registerEntry.RegisterTypeID);
-            parameters.Add("@WhyDeRegister", registerEntry.WhyDeRegisterID);
-            parameters.Add("@WhySecondDeRegister", registerEntry.WhySecondDeRegisterID);
-            parameters.Add("@SecondRegisterTypeID", registerEntry.SecondRegisterTypeID);
-            parameters.Add("@LandID", registerEntry.LandID);
-            parameters.Add("@NotaBene", registerEntry.NotaBene);
 
-            await connection.ExecuteAsync("dbo.uspSaveRegister", parameters, commandType: System.Data.CommandType.StoredProcedure);
-            return NoContent();
+            using (var connection = _connectionService.CreateConnection())
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("@RegisterID", entryId);
+                parameters.Add("@CustomerID", customerId);
+                parameters.Add("@FirstRegister", registerEntry.FirstRegister);
+                parameters.Add("@FirstDeregister", registerEntry.FirstDeregister);
+                parameters.Add("@SecondRegister", registerEntry.SecondRegister);
+                parameters.Add("@SecondDeRegister", registerEntry.SecondDeRegister);
+                parameters.Add("@Diagnosis", registerEntry.Diagnosis);
+                parameters.Add("@RegisterTypeID", registerEntry.RegisterTypeID);
+                parameters.Add("@WhyDeRegister", registerEntry.WhyDeRegisterID);
+                parameters.Add("@WhySecondDeRegister", registerEntry.WhySecondDeRegisterID);
+                parameters.Add("@SecondRegisterTypeID", registerEntry.SecondRegisterTypeID);
+                parameters.Add("@LandID", registerEntry.LandID);
+                parameters.Add("@NotaBene", registerEntry.NotaBene);
+
+                await connection.ExecuteAsync("dbo.uspSaveRegister", parameters, commandType: System.Data.CommandType.StoredProcedure);
+                return NoContent();
+            }
+
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new { message = $"{ex.Message}" });
         }
     }
 
