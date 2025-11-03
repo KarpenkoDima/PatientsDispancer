f74ae73b397c6b5301450e460472a640d5661d5b
Dima Karpenko dimak <dimka2010@gmail.com>
2025-11-03 08:56:32 +0200

    Create Login form. Add sln

---
+++ Dispancer.Frontend/Models/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Dispancer.Frontend.Models
+{
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Dispancer.Frontend/Models/LoginResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Dispancer.Frontend.Models;
+
+public class LoginResponse
+{
+    [JsonPropertyName("token")]
+    public string Token { get; set; }
+
+    [JsonPropertyName("userName")]
+    public string UserName { get; set; }
+
+    [JsonPropertyName("role")]
+    public string Role { get; set; }
+
+    [JsonPropertyName("sensitiveLevel")]
+    public string SensitiveLevel { get; set; }
+}
