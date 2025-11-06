e5bc63c887dc5fbcbd1a670bc017c9113445511b
Dima Karpenko dimak <dimka2010@gmail.com>
2025-11-06 14:15:30 +0200

    Refactoring AuthController and AuthService

--- Dispancer.Service/AuthService.cs
+++ Dispancer.Service/AuthService.cs
@@ -1,27 +1,87 @@
-﻿using Dispancer.SqlData;
+﻿using Dispancer.Core.Interfaces;
+using Dispancer.SqlData;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Dispancer.Service;
 
 public class AuthService
 {
     private readonly ISqlData _sqlData;
-    public AuthService(ISqlData sqlData)
+    private readonly ITokenService _tokenService;
+    private readonly IMemoryCache _memoryCache;
+    public AuthService(ISqlData sqlData, ITokenService tokenService, IMemoryCache memoryCache)
     {
         _sqlData = sqlData;
+        _tokenService = tokenService;
+        _memoryCache = memoryCache;
     }
-    public async Task<IEnumerable<string>> Login(string username, string password)
+    public async Task<(string Token, string DisplayRole, string SensitivityLevel, string role)> Login(string username, string password)
+    {
+        // 1. Аутентификация и получение ролей
+        try
+        {
+            var roles = await GetRolesAsync(
+                username,
+                password);
+
+
+            if (false == roles.Any())
+            {
+                throw new UnauthorizedAccessException("User has no roles assigned");
+            }
+          
+            // 2. Кешируем
+            var cacheEntryOptions = new MemoryCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromHours(1));
+            _memoryCache.Set(username, password, cacheEntryOptions);            
+
+            // 3. Определяем "главную" роль для отображения в БД
+            string displayRole = "Регистратор";
+            string role = "";
+            if (roles.Contains("Sensitive_high"))
+            {
+                displayRole = "Администратор";
+                role = "Sensitive_high";
+            }
+            else if (roles.Contains("Sensitive_medium"))
+            {
+                displayRole = "Оператор";
+                role = "Sensitive_medium";
+            }
+            else if (roles.Contains("Sensitive_low"))
+            {
+                role = "Sensitive_low";
+            }
+
+            // 4. Генерируем JWT
+            var token = _tokenService.GenerateToken(username, roles);
+
+            return (
+                token,
+                username,
+                displayRole,
+                role // Берем "старшую" роль
+            );
+        }
+        catch (SqlException ex)
+        {
+            throw ex;
+        }
+    }
+
+    private async Task<IEnumerable<string>> GetRolesAsync(string username, string password)
     {
         // 1. Пытаемся подключиться к БД с учетными данными пользователя
         try
-        {           
-                var roles = await _sqlData.LoginAsync(
-                    username, 
-                    password,
-                    "dbo.uspGetRoleForUser",               
-                    System.Data.CommandType.StoredProcedure);
-              
-                return roles;            
+        {
+            var roles = await _sqlData.LoginAsync(
+                username,
+                password,
+                "dbo.uspGetRoleForUser",
+                System.Data.CommandType.StoredProcedure);
+
+            return roles;
         }
         catch (SqlException ex)
         {
--- Dispancer/Controllers/AuthController.cs
+++ Dispancer/Controllers/AuthController.cs
@@ -35,64 +35,30 @@ public class AuthController : Controller
     [HttpPost("login")]
     public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
     {
-
         try
         {
+            var (token, displayRole, sensitivityLevel, role) = await _authService.Login(loginRequest.Username, loginRequest.Password);
 
-            var roles = await _authService.Login(loginRequest.Username, loginRequest.Password);
-
-            if (false == roles.Any())
-            {
-                return Unauthorized(new { message = "User has no roles assigned" });
-            }
-            // 4.4. -- НАЧАЛО БЛОКА ДЛЯ ДОБАВЛЕНИЯ --
-            // Сохраненеие пароля в кэш. Ключ - имя пользователя.
-            // пароль юудет храниться в кэше 1 час (можно настроить)
-            if (loginRequest.Password == null)
-            {
-                throw new InvalidOperationException("Сессия истекла. Пожалуйста, войдите снова.");
-            }
-            var cacheEntryOptions = new MemoryCacheEntryOptions()
-                .SetSlidingExpiration(TimeSpan.FromHours(1));
-            _memoryCache.Set(loginRequest.Username, loginRequest.Password, cacheEntryOptions);
-            // -- КОНЕЦ БЛОКА ДЛЯ ДОБАВЛЕНИЯ --
-
-            //  Определяем "главную" роль для отображения в БД
-            string displayRole = "Регистратор";
-            string role = "";
-            if (roles.Contains("Sensitive_high"))
-            {
-                displayRole = "Администратор";
-                role = "Sensitive_high";
-            }
-            else if (roles.Contains("Sensitive_medium"))
-            {
-                displayRole = "Оператор";
-                role = "Sensitive_medium";
-            }
-            else if (roles.Contains("Sensitive_low"))
-            {
-                role = "Sensitive_low";
-            }
-
-            // 4. Генерируем JWT
-            var token = _tokenService.GenerateToken(loginRequest.Username, roles);
 
             var responseData = new
             {
                 token = token,
                 userName = loginRequest.Username,
                 role = displayRole,
-                sensitivityLevel = role // Берем "старшую" роль
+                sensitivityLevel = role
             };
             return Ok(responseData);
 
         }
+        catch (UnauthorizedAccessException ex)
+        {
+            // Обработка случая, когда AuthenticateAndGenerateTokenAsync бросает исключение из-за отсутствия ролей
+            return Unauthorized(new { message = ex.Message });
+        }
         catch (SqlException ex)
         {
-            // Логируем другие возможные ошибки
+            // Логируем возможные ошибки
             // logger.LogError(ex, "An error occured during login");
-
             return StatusCode(500, new { mesage = $"An internal server error occurred. {ex.Message}" });
         }
     }
